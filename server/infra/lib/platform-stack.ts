d5f3fd5194a7022d5df8501650b898702fbd04c9
John Stephens <john.stephens95@gmail.com>
2025-08-13 15:14:54 -0700

    Add opensearch domains, get logs from CW into them via firehose

--- server/infra/lib/platform-stack.ts
+++ server/infra/lib/platform-stack.ts
@@ -21,6 +21,11 @@ import { DB_NAME, SERVER_NAME } from './constants'
 import { Repository } from 'aws-cdk-lib/aws-ecr'
 import { AppStage } from '../bin/infra'
 import { isDev } from './helpers'
+import {
+  Domain,
+  EngineVersion,
+  TLSSecurityPolicy,
+} from 'aws-cdk-lib/aws-opensearchservice'
 
 export interface PlatformStackProps extends StackProps {
   vpc: Vpc
@@ -31,6 +36,7 @@ export class PlatformStack extends Stack {
   public readonly dbEndpoint: string
   public readonly dbSecurityGroupId: string
   public readonly serviceRepo: Repository
+  public readonly opensearchDomain: Domain
 
   constructor(scope: Construct, id: string, props: PlatformStackProps) {
     super(scope, id, props)
@@ -94,6 +100,32 @@ export class PlatformStack extends Stack {
         ? RemovalPolicy.DESTROY
         : RemovalPolicy.RETAIN,
       lifecycleRules: [{ maxImageCount: 20 }],
+    })
+
+    // OpenSearch domain for logs (one per stage)
+    const domain = new Domain(this, 'ItoLogsDomain', {
+      domainName: `${stageName}-ito-logs`,
+      version: EngineVersion.OPENSEARCH_2_13,
+      enforceHttps: true,
+      nodeToNodeEncryption: true,
+      encryptionAtRest: { enabled: true },
+      ebs: { enabled: true, volumeSize: isDev(stageName) ? 20 : 50 },
+      capacity: {
+        dataNodes: 2,
+        dataNodeInstanceType: isDev(stageName)
+          ? 't3.small.search'
+          : 't3.medium.search',
+      },
+      zoneAwareness: { enabled: true, availabilityZoneCount: 2 },
+      tlsSecurityPolicy: TLSSecurityPolicy.TLS_1_2,
+    })
+    domain.applyRemovalPolicy(
+      isDev(stageName) ? RemovalPolicy.DESTROY : RemovalPolicy.RETAIN,
+    )
+    this.opensearchDomain = domain
+
+    new CfnOutput(this, 'OpenSearchEndpoint', {
+      value: domain.domainEndpoint,
     })
 
     Tags.of(this).add('Project', 'Ito')

7d4dc0228cc4ae27f3bdecc5ac1fb7d289cd031a
John Stephens <john.stephens95@gmail.com>
2025-09-24 14:52:08 -0700

    Updates

--- lib/media/native-interface.ts
+++ lib/media/native-interface.ts
@@ -1,6 +1,7 @@
 import { app } from 'electron'
 import os from 'os'
 import { join } from 'path'
+import fs from 'fs'
 
 const platform = os.platform()
 const isDev = !app.isPackaged
@@ -31,7 +32,11 @@ const getTargetDir = (nativeModuleName: string): string | null => {
     if (platform === 'darwin') {
       return join(targetBase, 'universal')
     } else if (platform === 'win32') {
-      return join(targetBase, 'x86_64-pc-windows-gnu/release')
+      const msvcDir = join(targetBase, 'x86_64-pc-windows-msvc/release')
+      const gnuDir = join(targetBase, 'x86_64-pc-windows-gnu/release')
+      if (fs.existsSync(msvcDir)) return msvcDir
+      if (fs.existsSync(gnuDir)) return gnuDir
+      return gnuDir
     }
     // Fallback for unsupported dev platforms
     return null

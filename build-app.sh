a272dd25d581ec124684b83378e33d9a22e11562
Julian Gomez <julgmz@gmail.com>
2025-09-26 07:49:17 -0500

    Revert "experimental compilation"
    This reverts commit a38f59159ccdcc2919de24863566e70bd5641808.

--- build-app.sh
+++ build-app.sh
@@ -107,13 +107,13 @@ build_native_modules() {
     
     case $platform in
         "mac")
-            ./build-binaries-workspace.sh --mac --universal
+            ./build-binaries.sh --mac --universal
             ;;
         "windows")
-            ./build-binaries-workspace.sh --windows
+            ./build-binaries.sh --windows
             ;;
         "all")
-            ./build-binaries-workspace.sh --all --universal
+            ./build-binaries.sh --all --universal
             ;;
         *)
             print_error "Invalid platform: $platform. Use 'mac', 'windows', or 'all'"

216045206b00f7b3be37455c18bf75d1c49ecdee
Julian Gomez <julgmz@gmail.com>
2025-11-19 14:00:20 -0600

    cleanup

--- electron-builder.config.js
+++ electron-builder.config.js
@@ -15,7 +15,7 @@ const getMacResources = () =>
 
 const getWindowsResources = () =>
   nativeBinaries.map(binary => ({
-    from: `native/${binary}/target/x86_64-pc-windows-msvc/release/${binary}.exe`,
+    from: `native/target/x86_64-pc-windows-msvc/release/${binary}.exe`,
     to: `binaries/${binary}.exe`,
   }))
 
